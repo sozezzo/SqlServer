178e2cd176cf8938ad9979056da58ebbfa9fd34e
ms <miltonsozezzo@hotmail.com>
2021-02-03 10:35:40 -0500

    Update spWriteStringToFile.sql
    New version

--- spWriteStringToFile.sql
+++ spWriteStringToFile.sql
@@ -3,15 +3,15 @@
 *  Description : Write in text file
 *
 *  Syntax :
-*  EXEC #spWriteStringToFile @String = 'Hello Word!', @Path = 'C:\Temp\', @Filename = 'myfile.txt';
+*  EXEC #spWriteStringToFile @Text = 'Hello Word!', @File = 'C:\Temp\myfile.txt';
 *
 *  Info : OA procs can only be accessed with Sysadmin authority
 *
 */
-CREATE OR ALTER PROCEDURE #spWriteStringToFile
-( @String VARCHAR(MAX)
-, @Path VARCHAR(512)
-, @Filename VARCHAR(512)
+(
+  @File NVARCHAR(4000),
+  @Text NVARCHAR(MAX),
+  @Charset NVARCHAR(100) = 'UTF-8'
 )
 AS
 BEGIN
@@ -32,50 +32,25 @@ RECONFIGURE;
 GO
 */
 	SET NOCOUNT ON;
+BEGIN 
 
-	DECLARE @objFileSystem INT , @objTextStream INT , @objErrorObject INT , @strErrorMessage VARCHAR(1000) , @Command VARCHAR(1000) , @hr INT , @fileAndPath VARCHAR(512);
-	SET NOCOUNT ON;
-	SELECT @strErrorMessage = 'opening the File System Object';
-	EXECUTE @hr = sp_OACreate 'Scripting.FileSystemObject'
-	,                         @objFileSystem OUT;
+	DECLARE @OLE            INT 
 
-	SELECT @FileAndPath = @path + '\' COLLATE DATABASE_DEFAULT + @filename;
+	EXECUTE sp_OACreate 'ADODB.Stream',  @OLE OUTPUT
 
-	IF @HR = 0
-		SELECT @objErrorObject = @objFileSystem
-		,      @strErrorMessage = 'Creating file "' COLLATE DATABASE_DEFAULT + @FileAndPath + '"' COLLATE DATABASE_DEFAULT ;
-	IF @HR = 0
-		EXECUTE @hr = sp_OAMethod @objFileSystem
-		,                         'CreateTextFile'
-		,                         @objTextStream OUT
-		,                         @FileAndPath
-		,                         2
-		,                         True;
-	IF @HR = 0
-		SELECT @objErrorObject = @objTextStream
-		,      @strErrorMessage = 'writing to the file "' COLLATE DATABASE_DEFAULT + @FileAndPath + '"' COLLATE DATABASE_DEFAULT ;
-	IF @HR = 0
-		EXECUTE @hr = sp_OAMethod @objTextStream
-		,                         'Write'
-		,                         NULL
-		,                         @String;
-	IF @HR = 0
-		SELECT @objErrorObject = @objTextStream
-		,      @strErrorMessage = 'closing the file "' COLLATE DATABASE_DEFAULT + @FileAndPath + '"' COLLATE DATABASE_DEFAULT ;
-	IF @HR = 0
-		EXECUTE @hr = sp_OAMethod @objTextStream
-		,                         'Close';
-	IF @hr <> 0
-	BEGIN
-		DECLARE @Source VARCHAR(255) , @Description VARCHAR(255) , @Helpfile VARCHAR(255) , @HelpID INT;
-		EXECUTE sp_OAGetErrorInfo @objErrorObject
-		,                         @source OUTPUT
-		,                         @Description OUTPUT
-		,                         @Helpfile OUTPUT
-		,                         @HelpID OUTPUT;
-		SELECT @strErrorMessage = 'Error whilst ' + COALESCE(@strErrorMessage, 'doing something' COLLATE DATABASE_DEFAULT ) + ', ' COLLATE DATABASE_DEFAULT + COALESCE(@Description, '' COLLATE DATABASE_DEFAULT );
-		RAISERROR(@strErrorMessage, 16, 1);
-	END;
-	EXECUTE sp_OADestroy @objTextStream;
-	EXECUTE sp_OADestroy @objFileSystem;
-END;
+	EXECUTE sp_OASetProperty             @OLE,    'Type',             2                           --1 = binary, 2 = text
+	EXECUTE sp_OASetProperty             @OLE,    'Mode',             3                           --0 = not set, 1 read, 2 write, 3 read/write
+	EXECUTE sp_OASetProperty             @OLE,    'Charset',          @Charset                     
+	EXECUTE sp_OASetProperty             @OLE,    'LineSeparator',    'adLF'
+	EXECUTE sp_OAMethod                  @OLE,    'Open'  
+	EXECUTE sp_OAMethod                  @OLE,    'WriteText',        NULL,       @Text      --text method
+
+	--Commit data and close text stream
+	EXECUTE sp_OAMethod                  @OLE,    'SaveToFile',       NULL,       @File, 2   --1 = notexist 2 = overwrite
+	EXECUTE sp_OAMethod                  @OLE,    'Close'
+	EXECUTE sp_OADestroy                 @OLE
+
+	EXECUTE sp_OADestroy @OLE 
+
+END 
+GO
