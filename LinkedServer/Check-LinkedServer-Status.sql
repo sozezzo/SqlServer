3c8de9b87532745ebb2d79cf9242c15b6a424c78
ms <miltonsozezzo@hotmail.com>
2022-01-15 21:48:29 -0500

    Update Check-LinkedServer-Status.sql

--- LinkedServer/Check-LinkedServer-Status.sql
+++ LinkedServer/Check-LinkedServer-Status.sql
@@ -1,30 +1,43 @@
 -- it identifies the linked servers, and verifies that they still work.
 
 SET NOCOUNT ON;
-DROP TABLE IF EXISTS #LinkedServers;
-SELECT * INTO #LinkedServers from [sys].[servers];
+drop table if exists #tbLinkedServer
+go
 
-DROP TABLE IF EXISTS #LinkedServersStatus;
-CREATE TABLE #LinkedServersStatus (server_id int, [name] sysname, [Status] int, [StatusDescription] varchar(128));
-DECLARE @retval int = 0, @sysservername sysname;
+SET NOCOUNT ON;
 
-DECLARE @server_id INT = 0;
-WHILE ((SELECT COUNT(*) FROM #LinkedServers) > 0)
-BEGIN
+DECLARE @tb table (CATALOG_NAME nvarchar(128), [DESCRIPTION] nvarchar(255));
+DECLARE @retval int = 0;
+DECLARE @linked_server_name sysname;
+DECLARE @sql nvarchar(max);
 
-  SELECT TOP 1 @server_id = server_id, @sysservername = CONVERT(sysname, [name]) FROM #LinkedServers ORDER BY server_id ASC;
-	DELETE FROM #LinkedServers WHERE @server_id = server_id;
+select
+	getutcdate() as [time],
+	@@servername as server_name,
+    srv.name as linked_server_name,
+    srv.data_source,
+	-1 as [status],
+	cast('' as nvarchar(1024)) as [status_desc]
+into #tbLinkedServer
+from
+    sys.servers srv
+where is_linked = 1;
 
+while ((select count(*) from #tbLinkedServer where [status] = -1) > 0)
+begin
+
+	select @linked_server_name = linked_server_name  from #tbLinkedServer where [status] = -1;
 	BEGIN TRY
-		EXEC @retval = sys.sp_testlinkedserver @sysservername;
-		INSERT INTO #LinkedServersStatus (server_id, [name], [Status], [StatusDescription])
-		VALUES (@server_id, @sysservername, 1, 'OK');
+		EXEC @retval = sys.sp_testlinkedserver @linked_server_name;
+		SET @sql = 'exec sp_catalogs @server_name = '''+@linked_server_name+''';'
+		INSERT INTO @tb EXEC (@sql);
+		update #tbLinkedServer set [status] = 1, [status_desc] = 'OK' where  linked_server_name = @linked_server_name
 	END TRY
 	BEGIN CATCH
-		INSERT INTO #LinkedServersStatus (server_id, [name], [Status], [StatusDescription])
-		VALUES (@server_id, @sysservername, 0, 'FAIL');
-	END CATCH;      
+		update #tbLinkedServer set [status] = 0, [status_desc] = ERROR_MESSAGE() where  linked_server_name = @linked_server_name
+	END CATCH; 
 
-END
-SELECT @@servername as 'ServerName', GetUtcdate() as DateUtc, server_id, [name] as LinkedServerName, [Status], [StatusDescription] FROM #LinkedServersStatus;
-GO
+end
+
+select [time], server_name, linked_server_name, data_source, [status], [status_desc] from #tbLinkedServer;
+
